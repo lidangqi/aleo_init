47a2587bb32215e3ebc55e11af5418071a9fb3aa
lidangqi <46198753+lidangqi@users.noreply.github.com>
2022-12-01 19:35:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,16 @@ echo "安装必要的工具"
     sudo apt install nvtop -y
 
 echo "开始安装rust"
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
-	source $HOME/.cargo/env
-	echo "rust安装成功!"
-	
+if [ ! "$(command -v rustc)" ]
+    then
+        echo "rust没有安装,开始安装rust"
+            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
+            source $HOME/.cargo/env
+            echo "rust安装成功!"
+    else
+            version=`rustc -V`
+            echo "rust已安装! ${version}"
+fi	
 echo "创建work目录"
     mkdir -p /root/miner
 
@@ -40,6 +46,8 @@ if [ ! "$(command -v rustc)" ]
           tmux has-session -t gpu01
           if [ $? != 0 ]
 	      then
+            read -p "请输入account_name > " account_name
+            echo "启动miner"
 		    tmux new-session -s gpu01 -d
 		    tmux new-session -s gpu02 -d
 		    tmux new-session -s gpu03 -d
@@ -48,17 +56,17 @@ if [ ! "$(command -v rustc)" ]
 		    tmux new-session -s gpu06 -d
 		    sleep 5
 
-		    tmux send-keys -t gpu01 'export CUDA_VISIBLE_DEVICES=0 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu01 > aleo0.log 2>&1' C-m
+		    tmux send-keys -t gpu01 'export CUDA_VISIBLE_DEVICES=0 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name $account_name --miner_name gpu01 > aleo0.log 2>&1' C-m
 		    sleep 5
-		    tmux send-keys -t gpu02 'export CUDA_VISIBLE_DEVICES=1 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu02 > aleo1.log 2>&1' C-m
+		    tmux send-keys -t gpu02 'export CUDA_VISIBLE_DEVICES=1 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name $account_name --miner_name gpu02 > aleo1.log 2>&1' C-m
 		    sleep 5
-		    tmux send-keys -t gpu03 'export CUDA_VISIBLE_DEVICES=2 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu03 > aleo2.log 2>&1' C-m
+		    tmux send-keys -t gpu03 'export CUDA_VISIBLE_DEVICES=2 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name $account_name --miner_name gpu03 > aleo2.log 2>&1' C-m
 		    sleep 5
-		    tmux send-keys -t gpu04 'export CUDA_VISIBLE_DEVICES=3 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu04 > aleo3.log 2>&1' C-m
+		    tmux send-keys -t gpu04 'export CUDA_VISIBLE_DEVICES=3 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name $account_name --miner_name gpu04 > aleo3.log 2>&1' C-m
 		    sleep 5
-		    tmux send-keys -t gpu05 'export CUDA_VISIBLE_DEVICES=4 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu05 > aleo4.log 2>&1' C-m
+		    tmux send-keys -t gpu05 'export CUDA_VISIBLE_DEVICES=4 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name $account_name --miner_name gpu05 > aleo4.log 2>&1' C-m
 		    sleep 5
-		    tmux send-keys -t gpu06 'export CUDA_VISIBLE_DEVICES=5 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu06 > aleo5.log 2>&1' C-m
+		    tmux send-keys -t gpu06 'export CUDA_VISIBLE_DEVICES=5 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name $account_name --miner_name gpu06 > aleo5.log 2>&1' C-m
               else
                      echo "tmux 运行中：${Name}"
           fi
