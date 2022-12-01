b8d14ff284b255c463be661173542077eacc093a
lidangqi <46198753+lidangqi@users.noreply.github.com>
2022-12-01 17:02:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,33 +41,23 @@ if [ ! "$(command -v rustc)" ]
           if [ $? != 0 ]
 	      then
 		    tmux new-session -s gpu01 -d
-		    sleep 5
 		    tmux new-session -s gpu02 -d
-		    sleep 5
 		    tmux new-session -s gpu03 -d
-		    sleep 5
 		    tmux new-session -s gpu04 -d
-		    sleep 5
 		    tmux new-session -s gpu05 -d
-		    sleep 5
 		    tmux new-session -s gpu06 -d
 		    sleep 5
 
 		    tmux send-keys -t gpu01 'export CUDA_VISIBLE_DEVICES=0 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu01
 		    ' C-m
-		    sleep 5
 		    tmux send-keys -t gpu02 'export CUDA_VISIBLE_DEVICES=1 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu02
 		    ' C-m
-		    sleep 5
 		    tmux send-keys -t gpu03 'export CUDA_VISIBLE_DEVICES=2 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu03
 		    ' C-m
-		    sleep 5
 		    tmux send-keys -t gpu04 'export CUDA_VISIBLE_DEVICES=3 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu04
 		    ' C-m
-		    sleep 5
 		    tmux send-keys -t gpu05 'export CUDA_VISIBLE_DEVICES=4 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu05
 		    ' C-m
-		    sleep 5
 		    tmux send-keys -t gpu06 'export CUDA_VISIBLE_DEVICES=5 && /root/miner/aleo-pool-prover_ubuntu1804_gpu-20221123 --pool 128.14.72.188:30009 --account_name 7h12 --miner_name gpu06
 		    ' C-m
               else
