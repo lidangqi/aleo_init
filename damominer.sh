cfe3fd81b91224e559f0927b676c1b63968925d4
lidangqi <46198753+lidangqi@users.noreply.github.com>
2022-12-03 00:16:42 +0800

    Update damominer.sh

--- damominer.sh
+++ damominer.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
+echo "开始安装rust"
+if [ ! "$(command -v rustc)" ]
+    then
+        echo "rust没有安装,开始安装rust"
+            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
+            source $HOME/.cargo/env
+            echo "rust安装成功!"
+    else
+            version=`rustc -V`
+            echo "rust已安装! ${version}"
+fi
 
-wget https://github.com/damomine/aleominer/releases/download/v1.3.0/damominer_v1.3.0.tar
+echo "下载锄头"
 
-mkdir -p /root/damominer1.3
-
-tar -xvf damominer_v1.3.0.tar -C /root/damominer1.3
-
-chmod +x /root/damominer1.3/damominer
+if [ -f /root/damominer1.3/damominer ]
+    then
+        cd /root/damominer1.3
+    else
+        mkdir -p /root/damominer1.3
+        wget https://github.com/damomine/aleominer/releases/download/v1.3.0/damominer_v1.3.0.tar
+        tar -xvf damominer_v1.3.0.tar -C /root/damominer1.3
+        chmod +x /root/damominer1.3/damominer
+        cd /root/damominer1.3
 
 read -p "请输入您的钱包地址 > " wallet
 
