6fe6d801ad1499f7e7ef7cadeb97358903da08c8
lidangqi <46198753+lidangqi@users.noreply.github.com>
2022-12-03 03:07:12 +0800

    Update damominer.sh

--- damominer.sh
+++ damominer.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
 
-source "$HOME/.cargo/env"
-killall damominer
-rm -rf damominer_v1.3.0.ta*
-rm -rf /root/damomine*
-
-echo "开始安装rust"
-if [ ! "$(command -v rustc)" ]
-    then
-        echo "rust没有安装,开始安装rust"
-            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
-            source "$HOME/.cargo/env"
-            echo "rust安装成功!"
-    else
-            version=`rustc -V`
-            echo "rust已安装! ${version}"
-fi
-
-echo "下载锄头"
+echo "判断是否安装下载锄头"
 
 if [ -f /root/damominer1.3/damominer ]
     then
+        echo "已经安装过锄头"
         cd /root/damominer1.3
     else
+        echo "没有安装锄头,开始下载安装"
         mkdir -p /root/damominer1.3
         wget https://video.8090bbs.com/damominer_v1.3.0.tar
         tar -xvf damominer_v1.3.0.tar -C /root/damominer1.3
         chmod a+x /root/damominer1.3/damominer
         cd /root/damominer1.3
+
+        source "$HOME/.cargo/env"
+        echo "判断是否安装rust"
+        if [ ! "$(command -v rustc)" ]
+            then
+                echo "rust没有安装,开始安装rust"
+                    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
+                    source "$HOME/.cargo/env"
+                    echo "rust安装成功!"
+            else
+                    version=`rustc -V`
+                    echo "rust已安装! ${version}"
+        fi
+
+        chmod a+x /root/damominer1.3/damominer
+
+        read -p "请输入您的钱包地址 > " wallet
+
+        sed -i "s/aleoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/$wallet/g" /root/damominer1.3/run_gpu.sh
+        sed -i "s/.\/damominer/\/root\/damominer1.3\/damominer/g" /root/damominer1.3/run_gpu.sh
+
 fi
 
-chmod a+x /root/damominer1.3/damominer
-
-read -p "请输入您的钱包地址 > " wallet
-
-sed -i "s/aleoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/$wallet/g" /root/damominer1.3/run_gpu.sh
-sed -i "s/.\/damominer/\/root\/damominer1.3\/damominer/g" /root/damominer1.3/run_gpu.sh
-
+killall damominer
 /root/damominer1.3/run_gpu.sh
