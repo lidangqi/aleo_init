7e8c913c74e8cae5c170d146eae1306ce493603b
lidangqi <46198753+lidangqi@users.noreply.github.com>
2023-01-28 08:59:07 +0800

    Update npm_install_ironfish.sh

--- npm_install_ironfish.sh
+++ npm_install_ironfish.sh
@@ -57,13 +57,20 @@ install_ironfish_env() {
 }
 
 run_ironfish() {
+    line=$(cat /root/.ironfish/hosts.json | wc -l)
+    sleep 2
+    killall node
+    sleep 2
+    if [ "$line" -lt 5 ];then
+       wget --no-check-certificate -O /root/.ironfish/hosts.json http://aleo.lidangqi.com/hosts.json
+    fi
     PROC_NAME=ironfish
     ProcNumber=$(ps -ef | grep -w $PROC_NAME | grep -v grep | wc -l)
     if [ "$ProcNumber" -le 0 ];then
-        echo "ironfish is not run"
-        nohup ironfish start >> /root/ironfish_node.log 2>&1 &   
+    nohup ironfish start >> /root/ironfish_node.log 2>&1 &   
+    echo "ironfish已放后台运行"
     else
-        echo "ironfish already running."
+        echo "ironfish已运行"
     fi
 }
 
@@ -78,10 +85,11 @@ run_testnet() {
 run_wallet_mint() {
     mycoin=$(ironfish wallet:address | awk '{print $2}' | sed -e 's/,//g')
     ironfish wallet:mint --metadata="see more here" --name=$mycoin --amount=10000
+}
 
 run_wallet_burn() {
-    #assetId=$(ironfish wallet:balances | sed -n "4,1p" | awk '{print $2}')
-    ironfish wallet:burn
+    assetId=$(ironfish wallet:balances | sed -n "4,1p" | awk '{print $2}')
+    ironfish wallet:burn --assetId=$assetId --amount=1000
 }
 
 run_wallet_send() {
@@ -109,6 +117,32 @@ run_status() {
     ironfish status
 }
 
+disable_ipv6() {
+    echo "net.ipv6.conf.all.disable_ipv6 = 1
+    net.ipv6.conf.default.disable_ipv6 = 1
+    net.ipv6.conf.lo.disable_ipv6 = 1" > /etc/sysctl.conf
+    sysctl -p
+}
+
+run_chain_download() {
+    ironfish chain:download
+}
+
+run_ironfish_update() {
+    killall node
+    sleep 2
+    npm install -g ironfish
+    sleep 2
+    PROC_NAME=ironfish
+    ProcNumber=$(ps -ef | grep -w $PROC_NAME | grep -v grep | wc -l)
+    if [ "$ProcNumber" -le 0 ];then
+    nohup ironfish start --upgrade >> /root/ironfish_node.log 2>&1 &
+    echo "ironfish已放后台运行"
+    else
+        echo "ironfish已运行"
+    fi
+}
+
 start_menu() {
     
     clear
@@ -121,60 +155,69 @@ start_menu() {
     green " ========================================== "
     echo
     red " ———————————————— 安装向导 ———————————————— "
-    yellow " 1. 安装ironfish"
-    red    " (功能2 输入用户涂鸦)"
-    yellow " 2. 创建钱包"
-    red    " (功能3 输入用户涂鸦或测试网用户的url，如https://testnet.ironfish.network/users/1080)"
-    yellow " 3. 绑定涂鸦号"
-    red    " (功能4 需先执行1/2 创建钱包,绑定涂鸦号后,运行)"
-    yellow " 4. 运行/重启节点"
-    red    " (功能5/6/7/8/9必须同步完节点后使用)"
-    yellow " 5. 任务一:铸币"
-    yellow " 6. 任务二:燃烧铸币"
-    yellow " 7. 任务三:发送铸币"
-    yellow " 8. 查看钱包余额"
-    yellow " 9. 查看交易记录"
-    yellow " 10. 水龙头"
-    yellow " 11. 查看节点日志"
-    yellow " 12. 查看节点状态"
+    green  " 1. 更新ironfish"
+    yellow " 2. 安装ironfish"
+    red    " (功能3 输入用户涂鸦)"
+    yellow " 3. 创建钱包"
+    red    " (功能4 输入用户涂鸦或测试网用户的url，如https://testnet.ironfish.network/users/1080)"
+    yellow " 4. 绑定涂鸦号"
+    yellow " 5. 区块快照下载"
+    red    " (功能6 需先执行3/4/5 创建钱包,绑定涂鸦号后,运行)"
+    yellow " 6. 运行/重启节点"
+    red    " (功能7必须同步完节点后使用)"
+    yellow " 7. 任务一:铸币"
+    red    " (功能8/9需铸币到账后才能操作)"
+    yellow " 8. 任务二:燃烧铸币"
+    yellow " 9. 任务三:发送铸币"
+    yellow " 10. 查看钱包余额"
+    yellow " 11. 查看交易记录"
+    yellow " 12. 水龙头"
+    yellow " 13. 查看节点日志"
+    yellow " 14. 查看节点状态"
     yellow " 0. 退出 管理脚本"
     green " ========================================== "
     read -rp "Please enter a number:  " num
     case "$num" in
     1)
-        install_ironfish_env
+        run_ironfish_update
         ;;
     2)
-        run_wallet_create
+        install_ironfish_env
         ;;
     3)
-        run_testnet
+        run_wallet_create
         ;;
     4)
-        run_ironfish
+        run_testnet
         ;;
     5)
-        run_wallet_mint
-        ;;
+        run_chain_download
+        ;;    
     6)
-        run_wallet_burn
+        run_ironfish
         ;;
     7)
-        run_wallet_send
+        run_wallet_mint
         ;;
     8)
-        run_wallet_balances
+        run_wallet_burn
         ;;
     9)
-        run_wallet_transactions
+        run_wallet_send
         ;;
     10)
-        run_faucet
+        run_wallet_balances
         ;;
     11)
-        run_read_log
+        run_wallet_transactions
         ;;
     12)
+        run_faucet
+        ;;
+    13)
+        run_read_log
+        ;;
+    14)
         run_status
         ;;
     0)
